2fff3c212840f17591aebb9ede2716c2a1634b0f
meryngii <meryngii+git@gmail.com>
2013-04-19 23:43:15 +0900

    Changed filenames

--- install.bat
+++ install.bat
@@ -1,9 +1,11 @@
 
+if %HOME%=='' set %HOME%=%HOMEDRIVE%%HOMEPATH%
+
 git submodule init && git submodule update
 
 mkdir .vim\backup .vim\swp
 
-mklink /d %HOMEDRIVE%.vim .vim
-mklink .vimrc .vimrc
-mklink .gvimrc .gvimrc
-mklink .vimshrc .vimshrc
+mklink /d %HOME%\.vim     _vim
+mklink    %HOME%\.vimrc   _vimrc
+mklink    %HOME%\.gvimrc  _gvimrc
+mklink    %HOME%\.vimshrc _vimshrc
